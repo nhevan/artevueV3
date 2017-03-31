b30cbe499d05a927e4256a0003c5c7c07c331419
NH Evan <nhevan@gmail.com>
2017-03-31 21:03:08 +0600

    New api to update user info.

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,7 @@ use App\Mail\WelcomeEmail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use Acme\Transformers\UserTransformer;
+use Illuminate\Http\Response as IlluminateResponse;
 
 class UsersController extends ApiController
 {
@@ -221,5 +222,61 @@ class UsersController extends ApiController
         $users = $this->user->where('username', 'like', '%'.$search_string.'%')->orWhere('name', 'like', '%'.$search_string.'%')->orWhere('email', 'like', '%'.$search_string.'%')->with('usertype', 'metadata')->paginate($limit);
 
         return $this->respondWithPagination($users, $this->userTransformer);
+    }
+
+    /**
+     * updates user specific info like name, website, biography, user_type_id, email, phone, sex [art_preferences and art_types]
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function edit(Request $request)
+    {
+        $rules = [
+            'name' => 'required|max:50',
+            'email' => 'required|email',
+            'user_type_id' => 'required|not_in:1,2',
+            'website' =>'nullable|url',
+            'biography' => 'nullable|max:250',
+            'phone' => 'nullable',
+            'sex' => 'required',
+        ];
+        if (!$this->setRequest($request)->isValidated($rules)) {
+            return $this->responseValidationError();
+        }
+        if(!$this->emailIsUnique($request)){
+            return $this->setStatusCode(IlluminateResponse::HTTP_UNPROCESSABLE_ENTITY)->respondWithError([ 'message' => [ 'email' => 'The email has already been taken.' ] ] );
+        }
+
+        $user = $request->user();
+
+        $user->email = $request->email;
+        $user->name = $request->name;
+        $user->user_type_id = $request->user_type_id;
+        $user->website = $request->website;
+        $user->biography = $request->biography;
+        $user->phone = $request->phone;
+        $user->sex = $request->sex;
+        
+        $user->save();
+
+        return $this->respond( [ 'message' => 'The user has been updated.' ] );
+    }
+
+    /**
+     * returns TRUE ONlY IF the provided email is not taken by anyone else OR the user himself is the owner of the email
+     * @param  Request $request [description]
+     * @return [type]           [description]
+     */
+    public function emailIsUnique(Request $request)
+    {
+        $auth_user = $request->user()->toArray();
+        $email_owner = $this->user->where('email', $request->email)->first();
+        
+        if (!$email_owner) {
+            return true;
+        }
+        if ($auth_user == $email_owner->toArray()) {
+            return true;
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,12 +22,14 @@ Route::middleware('auth:api')->get('/test-user/{user}', function (User $user) {
 // Route::middleware('auth:api')->get('/user', 'UsersController@index');
 Route::middleware('auth:api')->get('/user/{user}', 'UsersController@show');
 Route::middleware('auth:api')->post('/user', 'UsersController@store');
+Route::middleware('auth:api')->put('/user', 'UsersController@edit');
 Route::middleware('auth:api')->get('/current-user', 'UsersController@currentUser');
-Route::middleware('auth:api')->get('/facebook-login', 'UsersController@facebookLogin');
-Route::middleware('auth:api')->post('/facebook-signup', 'UsersController@facebookSignup');
 Route::middleware('auth:api')->get('/fetch-user-id', 'UsersController@fetchUserIdByUsername');
 Route::middleware('auth:api')->get('/check-username', 'UsersController@checkUsername');
 Route::middleware('auth:api')->get('/search-user', 'UsersController@searchUser');
+Route::middleware('auth:api')->get('/facebook-login', 'UsersController@facebookLogin');
+Route::middleware('auth:api')->post('/facebook-signup', 'UsersController@facebookSignup');
+
 
 Route::middleware('auth:api')->get('/usertypes', 'UserTypesController@index');
 
