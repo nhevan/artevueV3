6fda3b6bd8cbe55f51a1271e3705b68015b6b773
NH Evan <nhevan@gmail.com>
2017-04-02 17:56:24 +0600

    New api to send messages.

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -2,9 +2,63 @@
 
 namespace App\Http\Controllers;
 
+use App\Message;
 use Illuminate\Http\Request;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Response as IlluminateResponse;
 
 class MessagesController extends ApiController
 {
-    //
+	/**
+	 * sends a message to a user
+	 * @param  Request $request [description]
+	 * @return [type]           [description]
+	 */
+    public function store(Request $request)
+    {
+    	$rules = [
+            'receiver_id' => 'required',
+            'message' => 'required'
+        ];
+        if (!$this->setRequest($request)->isValidated($rules)) {
+            return $this->responseValidationError();
+        }
+        if ($request->is_file == 1 && $request->is_post == 1) {
+    		return $this->respondWithError(['message'=>'A message can not be of both file and post type at the same time, Check your is_file and is_post key.']);
+    	}
+    	
+    	try {
+    		$this->createMessage($request);
+    	} catch (QueryException $e) {
+    		return $this->setStatusCode(IlluminateResponse::HTTP_UNPROCESSABLE_ENTITY)->respondWithError('Something went wrong, probably the receiver does not exist.');
+    	}
+    	
+        return $this->respond(['message'=>'Message successfully sent.']);
+    }
+
+    /**
+     * create new message instance
+     * @param  Request $request 
+     * @return response           
+     */
+    public function createMessage(Request $request)
+    {
+    	$message = new Message;
+    	$message->sender_id = $request->user()->id;
+    	$message->receiver_id = $request->receiver_id;
+    	$message->message = $request->message;
+
+    	if ($request->is_file) {
+    		$message->is_file = 1;
+    		$message->url = $request->file('url')->store(
+	            'img/messages', 's3'
+	        );
+    	}
+    	if ($request->is_post) {
+    		$message->is_post = 1;
+    		$message->url = $request->url;
+    	}
+
+		$message->save();    	
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -44,3 +44,4 @@ Route::middleware('auth:api')->post('/report/{user_id}', 'ReportedUsersControlle
 
 Route::middleware('auth:api')->get('/usertypes', 'UserTypesController@index');
 
+Route::middleware('auth:api')->post('/message', 'MessagesController@store');
