a84ca757b6df022bad9f86b8cfaf99a9cbcb1098
NH Evan <nhevan@gmail.com>
2017-04-15 02:05:55 +0600

    fix-2 welcome email template

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -16,19 +16,12 @@ ArteVue is about making the art world accessible to everyone, anywhere in the wo
 # ArteVue places you in the center of your art world:
 
 * Share, follow, discover, like and comment on art at a global level
-
 * Interact through personal messages or just follow artists, collectors, galleries, fairs etc, based on the art you like
-
 * Take a picture, tag, scan a label, comment, upload and share on multiple social platforms instantly
-
 * Build a virtual art collection, which you have, or just like! You don’t have to be a collector to have an art collection
-
 * Label, document, curate and create a PDF catalogue of your collection to share with friends or clients
-
 * Be directly in touch with art professionals for advice or to just to find out which art fairs to go to
-
-* Buy/ sell and even trade artworks by interacting with artists, collectors, galleries and professionals through the App
-
+* Buy/sell and even trade artworks by interacting with artists, collectors, galleries and professionals through the App
 * Keep ahead of the latest developments globally through our art daily news and events feed
 <br>
 <br>
