6b8b47d35f62c2fc993232d995ebf187f3d86798
NH Evan <nhevan@gmail.com>
2017-04-15 03:31:18 +0600

    HATE change, i need to sleep :(

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,5 +1,5 @@
 <tr>
-    <td class="header">
+    <td class="header" style="background-color: #fff; border-bottom: 5px solid #000;">
         <a href="{{ $url }}">
 	        <img style="width:200px;" src="http://dy01r176shqrv.cloudfront.net/artevue-logo-black-hr.png">
         </a>
