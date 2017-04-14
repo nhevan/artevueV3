c7146ba3cf91e7bba862d720a2965770345b7cf8
NH Evan <nhevan@gmail.com>
2017-04-15 02:25:39 +0600

    fix-3 welcome email template

--- resources/views/emails/welcome.blade.php
+++ resources/views/emails/welcome.blade.php
@@ -23,8 +23,7 @@ ArteVue is about making the art world accessible to everyone, anywhere in the wo
 * Be directly in touch with art professionals for advice or to just to find out which art fairs to go to
 * Buy/sell and even trade artworks by interacting with artists, collectors, galleries and professionals through the App
 * Keep ahead of the latest developments globally through our art daily news and events feed
-<br>
-<br>
+
 Thank you and we look forward to seeing your art.
 <br>
 <br>
