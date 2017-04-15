520f2ac13e4ca87de40c20e0a69c8b753f328184
NH Evan <nhevan@gmail.com>
2017-04-15 18:22:16 +0600

    Update user block method and implement removing all pins of blocked user and vice versa.

--- app/Http/Controllers/BlockedUsersController.php
+++ app/Http/Controllers/BlockedUsersController.php
@@ -2,15 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Pin;
+use App\Post;
 use App\User;
 use App\BlockedUser;
 use Illuminate\Http\Request;
 use App\Traits\UserSwissKnife;
+use App\Traits\CounterSwissKnife;
 use Illuminate\Support\Facades\Auth;
 
 class BlockedUsersController extends ApiController
 {
-	use UserSwissKnife;
+	use UserSwissKnife, CounterSwissKnife;
 
     protected $blockedUser;
 
@@ -35,8 +38,8 @@ class BlockedUsersController extends ApiController
         	return $this->unBlockUser($user_id);
         }
 
+        $this->removeALlPinsOfBlockedUser($user_id);
         $this->removeBlockedUserFromFollowerList($user_id);
-        $this->removeALlPinsOfBlockedUser($user_id); //unimplemented
 
         return $this->blockUser($user_id);
     }
@@ -80,9 +83,38 @@ class BlockedUsersController extends ApiController
      * removes all pins {posts pinned by current user that was created by blocked user and Vice Versa}
      * @return [type] [description]
      */
-    public function removeALlPinsOfBlockedUser($user_id)
+    public function removeALlPinsOfBlockedUser($blocked_user_id)
     {
-    	return true;
+        $current_user_post_ids = $this->getUserPosts(Auth::user()->id);
+        $this->removePinsOfPosts($current_user_post_ids, $blocked_user_id);
+
+        $blocked_user_post_ids = $this->getUserPosts($blocked_user_id);
+        $this->removePinsOfPosts($blocked_user_post_ids, Auth::user()->id);
+    }
+
+    /**
+     * removes all pins of a user within a given set of post_ids
+     * @param  array $post_ids contains an array of post ids
+     * @param  integer $user_id  the user whose pins needs to be removed.
+     * @return [type]           [description]
+     */
+    private function removePinsOfPosts($post_ids, $user_id)
+    {
+        $pins = Pin::whereIn('post_id', $post_ids)->where('user_id', $user_id)->get();
+        foreach ($pins as $pin) {
+            $this->decrementUserPinCount($user_id);
+            $pin->delete();
+        }
+    }
+
+    /**
+     * get ids of all posts of a given user_id
+     * @param  [type] $user_id [description]
+     * @return [type]          [description]
+     */
+    public function getUserPosts($user_id)
+    {
+        return Post::where('owner_id', $user_id)->pluck('id')->toArray();
     }
 
     /**
