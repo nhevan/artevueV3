493a2d8353eda5182f5a731a6c6a875abdf6d1bd
NH Evan <nhevan@gmail.com>
2017-07-06 17:25:32 +0600

    Add new footer section to Notify Issue Email template.

--- resources/views/emails/notify_issue.blade.php
+++ resources/views/emails/notify_issue.blade.php
@@ -21,4 +21,10 @@ Sho Ahad-Choudhury<br>
 <strong>Founder and Chief Executive Officer</strong><br>
 Email : <hello@artevue.co.uk><br>
 Website : [www.artevue.co.uk](www.artevue.co.uk)
+<br>
+<br>
+This email and any attachment is confidential, may be legally privileged and must not be disclosed to or used by anyone other than the intended recipient. Unauthorised use, disclosure, distribution or copying is prohibited and may be unlawful. If you are not the intended recipient, please notify <hello@artevue.co.uk> and then delete this email. 
+<br>
+<br>
+This email is sent over a public network and its completeness or accuracy cannot be guaranteed. Any attached files were checked with virus detection software before sending, but you should carry out your own virus check before opening them and we do not accept liability for any loss or damage caused by software viruses.
 @endcomponent
