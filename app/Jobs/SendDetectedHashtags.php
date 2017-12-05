edac0dd7e649bcc2cca80a961e83bb1d69a3e181
NH Evan <nhevan@gmail.com>
2017-12-05 17:32:27 +0600

    Fix a issue where detected hashtags response was too slow as it was pushed to queue rather than handled instantly.

--- app/Jobs/SendDetectedHashtags.php
+++ app/Jobs/SendDetectedHashtags.php
@@ -13,7 +13,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 
-class SendDetectedHashtags implements ShouldQueue
+class SendDetectedHashtags
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels, NotificationSwissKnife;
     
