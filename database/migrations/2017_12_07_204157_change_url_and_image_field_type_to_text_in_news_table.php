e3e9a9b97f231d78907b33763528c4d0f77568ae
NH Evan <nhevan@gmail.com>
2017-12-08 02:45:11 +0600

    Fix some fields in news and events table.

---
+++ database/migrations/2017_12_07_204157_change_url_and_image_field_type_to_text_in_news_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeUrlAndImageFieldTypeToTextInNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->text('image')->change();
+            $table->text('url')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('news', function (Blueprint $table) {
+            $table->string('image');
+            $table->string('url');
+        });
+    }
+}
---
+++ database/migrations/2017_12_07_204351_change_url_and_image_field_type_to_text_in_events_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeUrlAndImageFieldTypeToTextInEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->text('image')->change();
+            $table->text('url')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            $table->string('image');
+            $table->string('url');
+        });
+    }
+}
